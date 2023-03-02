3385462bb21ee43ac4c5020bb4db4c7513201ea6
Tetsuo Seto <setogit@gmail.com>
2023-03-01 23:32:26 -0800

    Disable stats; browser.gatherUsageStats to False

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,7 @@ import pandas as pd
 import numpy as np
 import os, urllib, cv2
 
+os.environ["STREAMLIT_BROWSER_GATHER_USAGE_STATS"] = "0"
 st.set_page_config(layout="wide", page_title="YOLO", page_icon=":taxi:")
 
 # Streamlit encourages well-structured code, like starting execution in a main() function.
